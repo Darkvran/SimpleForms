a484da0306157a95d3bf974a29da0dad33fc95b3
Влад Буков <vladislav.voron.2017@mail.ru>
2023-12-06 23:09:09 +0300

    Выполнены условия ТЗ. Осталось дописать get метод

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const authRouter = require('./authRouter');
-const PORT = process.env.PORT || 5000;
+const app = require("./app"); 
+const port = process.env.PORT || 5000;
 
-const app = express(); 
-app.use(express.static("public"));
-
-const urlencodedParser = express.urlencoded({extended: false});
-app.get("/", function(_, response) {
-    response.sendFile(__dirname + "/public/index.html")
-});
+app.get('/', (req, res) => {
+    res.send(__dirname + "public/index.html")
+})
 
 
-app.use(express.json())
-app.use("/auth", authRouter);
-
-const start = async () => {
-    try {
-        await mongoose.connect(`mongodb://127.0.0.1:27017/NikitosTask`);
-        app.listen(PORT, () => console.log(`server started on port ${PORT}`));
-    }
-    catch (e) {
-        console.log(e);
-    }
-}
-
-start();
+app.listen(port, () => console.log (`Сервер запущен на ${port}`));
--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const {Schema, model} = require('mongoose');
 const User = new Schema({
     email: {type: String, unique: true, required: true},
-    username: {type: String, uniqie: true, required: true},
+    username: {type: String, required: true},
     password: {type: String, required: true}
 })
 
